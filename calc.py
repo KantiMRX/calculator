0809f01acebcd856d35c66403a0fe59e00d62ab8
KantiMRX <59829213+KantiMRX@users.noreply.github.com>
2020-04-02 16:15:30 +0430

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,22 @@
+print("პროგრამა შექმნილა Kanti-ის მიერ, ისიამოვნეთ.")
+x = "symbols"
+x = float(input("შეიტანეთ x: "))
+
+
+oper = input("აირჩიეთ ოპერაცია: +, -, /, * ...> ")
+
+
+y = float(input("აირჩიეთ y: "))
+    
+
+
+if oper == "+":
+    print(x + y)
+elif oper == "-":
+    print(x - y)
+elif oper == "/":
+    print(x / y)
+elif oper == "*":
+  print(x * y)
+else:
+    print("შეცდომა")
